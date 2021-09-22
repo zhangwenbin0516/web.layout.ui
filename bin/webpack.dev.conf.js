7bcb09163cecf2cea4c8497529cc6ce3d13aa148
zhangwenbin <zhangwenbin@yonghui.cn>
2021-09-22 18:59:39 +0800

    组件开发

---
+++ bin/webpack.dev.conf.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const WebpackConfig = require('./webpack.base');
+console.log(merge)
+module.exports = merge(WebpackConfig, {
+    mode: 'development',
+    entry: {
+        app: path.resolve(__dirname, '../index.js')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        host: 'localhost',
+        port: '3590',
+        open: true
+    }
+})
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+    return <div>asdad</div>
+}
+render(<App />, document.getElementById('YHUI'));
