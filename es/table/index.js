243485392124812d9085078f0a6a924b0bee8e08
zhangwenbin <zhangwenbin@yonghui.cn>
2021-09-28 11:06:35 +0800

    table

--- es/table/index.js
+++ es/table/index.js
@@ -1,5 +1,12 @@
 /**
  * @table 表格功能
  */
-import _typeof from '@babel/runtime/helpers/esm/typeof';
 import React from 'react';
+
+const Table = (props) => {
+    return React.createElement('div');
+}
+Table.defaultprops = {
+    rowKey: 'key'
+}
+export default Table;
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { render } from 'react-dom';
+import Table from '@es/table';
 
 const App = () => {
-    return <div>asdad</div>
+    useEffect(() => {
+        console.log(Table)
+    }, [])
+    return <div>
+        <Table />
+    </div>
 }
 render(<App />, document.getElementById('YHUI'));
